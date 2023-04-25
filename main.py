6b939595649e6fd1628a76602f0c86681a9c5bfd
felice1004 <palette1004@gmail.com>
2023-04-25 15:36:11 +0800

    add config & process response

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ if st.button(message):
         if query_name not in used_query_name:
             # new query name
             output[dict_key] = [0, query_url, query_name, []] #show times, url, query name, query results
-            output[dict_key][3].append('TEST')
+            # output[dict_key][3].append('TEST')
             print('hi')
             #request
             address = selected_country + " " + query_name
@@ -113,8 +113,8 @@ if st.button(message):
                 print('tried')
                 #只取搜尋結果前三個
                 for index, candidate in enumerate(candidates):
-                    output[dict_key][3].append('TEST')
-                    # output[dict_key][3].append(candidate['name'])
+                    # output[dict_key][3].append('TEST')
+                    output[dict_key][3].append(candidate['name'])
                     if index >= 3:
                         break
             except:
