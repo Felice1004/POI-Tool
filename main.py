d1ba2e8d23c8a4592f00367d5c873c2a71584415
felice1004 <palette1004@gmail.com>
2023-04-25 13:43:25 +0800

    add config & process response

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def get_colname_options(data):
     for col in data.columns:
         colnames.append(col)
     return colnames
-   
+
+
 st.title('Klook POI Extractor')
 
 #----------------------------------------#
@@ -39,9 +40,8 @@ colname_URL = st.selectbox(
 colname_POI = st.selectbox(
     '2.2 Choose the column that contains POI', colnames)
 
-results = dict()
+query_list = dict()
 i = 0
-
 try:
     data = data[data[colname_POI].notnull()] #清除值為NAN的資料
 
@@ -50,52 +50,65 @@ try:
             # print(type(poi), poi)
             poi.replace("'",'')
             poi = poi.split(', ')
-            for attraction in poi:  
-                results[i] =  [url, attraction]
-                i = i + 1
+            for poi_name in poi:  
+                query_list[i] =  [url, poi_name]
 except:
     print('Something went wrong. ')
 
 
-st.header('3 - API Request')
-input_key = st.text_input('You API Key')
-selected_country = st.selectbox('Which country are you searching for?',('Option','Taiwan','Korea','Vietnam'))
-selected_region = st.selectbox('Which region are you searching for?',('Option','tw','kr','vn'))
-selected_language = st.selectbox('Which language do you want to return?',('Option','zh-TW','en'))
+st.header('3 - Confirm API Request Info')
+input_key = st.text_input(st.secrets("API_key"))
+selected_country = st.selectbox('Which country are you searching for?',st.secrets["country"])
+selected_language = st.selectbox('Which language do you want to return?',st.secrets["language"])
+
+####
 
 done = False
-
-if st.button('Execute '+ str(len(results))+ ' POIs'):
+used_query_name = set()
+output=dict()
+if st.button('Comfirm & Execute '+ str(len(query_list))+ ' POIs'):
     
     my_bar = st.progress(0, text='processing...')  
-    for k in results:
-        key = input_key 
-        lang = selected_language
-        location="region:"+selected_region
-        address = selected_country + " " + results[k][1]
+    for query_name in query_list:
+        if query_name not in used_query_name:
+            # new query name
+            output[query_name] = [0,[]] #show times, query results
 
-        url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?locationbias={location}&fields=name%2Cformatted_address&language={lang}&inputtype=textquery&input={address}&key={key}"
+            #request info
+            key = input_key 
+            lang = selected_language
+            loc="region:"+selected_country
+            address = selected_country + " " + query_list[query_name][1]
+            url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?locationbias={loc}&fields=name%2Cformatted_address&language={lang}&inputtype=textquery&input={address}&key={key}"
 
-        response = requests.get(url)
-        data = response.json()
-        candidates = data['candidates']
+            #send request
+            response = requests.get(url)
+            data = response.json()
+            candidates = data['candidates']
 
-        try:
-            results[k].append(candidates[0]['name'])
-            results[k].append(candidates[0]['formatted_address'])
-        except:
-            results[k].append("NOT FOUND")
-            print('fail', address)
-        my_bar.progress(k/len(results), text='processing...')
+            #process response
+            try:
+                output[query_name][0] += 1
+                for index, candidate in enumerate(candidates):
+                    output[query_name][1].append(candidate['name'])
+                    if index >= 3:
+                        break
+            except:
+                output[query_name][1].append("NOT FOUND")
+            finally:
+                used_query_name.add(query_name)
+        else:
+            #發過query的就不要再發了，直接次數＋1就好
+            output[query_name][0] += 1
+        my_bar.progress(query_name/len(query_list), text='processing...')
 
-    done = True
+    done = True #是否顯示下載按鈕
 import csv
 
 if done:
-    csv_data = dict_to_csv(results)
-
+    csv_data = dict_to_csv(output)
     st.download_button(
     label="Download Result",
     data=csv_data,
-    file_name='results.csv',
+    file_name='query output.csv',
     mime='text/csv')
